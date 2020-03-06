c2bff07f301a3616a2d2c0fd148df71e83312a69
gkgg123 <gkggg123@gmail.com>
2020-03-06 23:41:35 +0900

    공부 추가

---
+++ 알고리즘/swexpert_algo_problem/자바반 문제/swexpert_2814_최장경로.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin=open('2814.txt','r')
+
+T=int(input())
+
+def dfs(x,cnt):
+    global max_cnt
+    visited[x][x]=1
+    st=[[x,cnt]]
+    
+    while st:
+        a,cn=st.pop()
+        if max_cnt<cn:
+            max_cnt=cn
+        for i in graph[a]:
+            if visited[x][i]==0:
+                st.append([i,cn+1])
+                visited[x][i]=1
+                break
+                
+ 
+
+
+for tc in range(1,T+1):
+    N,M=list(map(int,input().split()))
+    graph=[[] for _ in range(N+1)]
+    max_cnt=1
+    for i in range(M):
+        x,y=list(map(int,input().split()))
+        graph[x].append(y)
+        graph[y].append(x)
+    visited=[[0]*(N+1) for _ in range(N+1)]
+    for i in range(1,N+1):        
+        dfs(i,1)
+
+    print('#{} {}'.format(tc,max_cnt))
